6c78ba47a3f38d8e0af1730c0d383671700ae067
LebeauQuentin <quentin.lebeau@oclock.school>
2025-01-27 18:23:55 +0100

    init depot Template React

---
+++ front/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      
+      <p> Hello World </p>
+
+      </div>
+    </>
+  )
+}
